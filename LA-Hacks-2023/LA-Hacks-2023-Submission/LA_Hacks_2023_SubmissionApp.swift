64731905589cec7646870fcfdb1936357fe35d44
JohnHouser <email.johnhouser@gmail.com>
2023-04-22 23:00:51 -0700

    Restart from scratch attempt #2

---
+++ LA-Hacks-2023/LA-Hacks-2023-Submission/LA_Hacks_2023_SubmissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LA_Hacks_2023_SubmissionApp.swift
+//  LA-Hacks-2023-Submission
+//
+//  Created by John Houser on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct LA_Hacks_2023_SubmissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
